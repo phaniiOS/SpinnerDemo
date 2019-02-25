20abd3c1c9847d5a71f37586f5316b34a14b5732
Phanindranath Thota <phanindranath.wg113@gmail.com>
2019-02-25 06:10:00 -0600

    Spinner Demo

--- SpinnerDemo/ViewController.swift
+++ SpinnerDemo/ViewController.swift
@@ -9,7 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let spinner = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+    
+    @IBAction func StartAction(_ sender: Any) {
+        spinner.center = view.center
+        spinner.style = UIActivityIndicatorView.Style.gray
+        view.addSubview(spinner)
+        spinner.startAnimating()
+    }
+    
+    
+    @IBAction func StopAction(_ sender: Any) {
+        spinner.stopAnimating()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
